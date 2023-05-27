be16d6402bc9d3067c52a96cb1323cfbc2b73dac
reinerkp <kate.reiner1@gmail.com>
2023-05-27 13:53:09 -0600

    added form for contact me

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Contact() {
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Perform form validation
+    // Send form data to the backend server for email processing
+
+    // Reset the form fields
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <h3>Contact Me</h3>
-    <ul>
-        <li><a href="mailto:kate.reiner1@gmail.com"> kate.reiner1@gmail.com</a></li>
-        <li> Phone Number: 303-815-5979</li>
-        <li><a href="https://github.com/reinerkp">Git Hub Portfolio</a></li>
-        <li><a href="https://www.linkedin.com/in/katherine-reiner/">linkedIn</a></li>
-    </ul>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={handleMessageChange}
+          required
+        ></textarea>
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   );
-}
+};
+
+export default ContactForm;
